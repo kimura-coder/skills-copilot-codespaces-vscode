b33e71d5277121a4ad1ee8a5e50821c2aebde324
Alfie <67652356+kimura-coder@users.noreply.github.com>
2024-10-18 08:22:03 +0000

    copilot fifth commit

--- comments.js
+++ comments.js
@@ -1,26 +1,17 @@
-// create web server
-var express = require('express');
-var app = express();
-// create server
-var server = require('http').createServer(app);
-// create socket.io
-var io = require('socket.io')(server);
-
-// create a connection
-io.on('connection', function(socket){
-  console.log('a user connected');
-  // listen to the event 'comment'
-  socket.on('comment', function(msg){
-    // emit the event 'comment' to all clients
-    io.emit('comment', msg);
-  });
-  // listen to the event 'disconnect'
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+// creeate web server 
+// 1. import express
+const express = require('express');
+// 2. create express server
+const app = express();
+// 3. define port number
+const port = 3000;
+// 4. define route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
+// 5. start server
+app.listen(port, () => {
+    console.log(`Server is running at http://
 
-// listen on port 3000
-server.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+
+
